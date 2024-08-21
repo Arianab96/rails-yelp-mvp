9d44f8c3913b770000e619d5333e2968c63baab9
Ariana Bakhshandeh <arianabakhshandeh@gmail.com>
2024-08-21 14:12:08 +0100

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+da_mario = { name: 'Da Mario', address: '15 Gloucester Rd, London SW7 4PP', phone_number: '02075849078', category: 'italian' }
+sumi = { name: 'Sumi', address: '157 Westbourne Grove, London W11 2RS', phone_number: '02045240880', category: 'japanese' }
+gold_mine = { name: 'Gold Mine', address: '100-102 Queensway, London W2 3RR', phone_number: '02077928331', category: 'chinese' }
+balthazar = { name: 'Balthazar', address: '4-6 Russell St, London WC2B 5HZ', phone_number: '02033011155', category: 'french' }
+min_jiang = { name: 'Min Jiang', address: '24 Kensington High St, London W8 4PT', phone_number: '02073611988', category: 'chinese' }
+
+[da_mario, sumi, gold_mine, balthazar, min_jiang].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
